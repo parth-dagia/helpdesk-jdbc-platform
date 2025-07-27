1360a858bda114abc72a59c671a4f79148ea32e6
Parth Dagia <parth.24bcs10414@sst.scaler.com>
2025-07-27 13:07:34 +0530

    Removed real DB password

--- util/DBUtil.java
+++ util/DBUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/helpdesk";
     private static final String USER = "root";
-    private static final String PASSWORD = "Parthdagia@123"; // ← Replace this
+    private static final String PASSWORD = "enter your password"; // ← Replace this
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
